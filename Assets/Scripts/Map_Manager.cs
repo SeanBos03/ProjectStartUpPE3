7172c1fcd69bd2ba7dfbfefce9dabe013d640872
Lluis Alguersuari <lluisalguersuari@gmail.com>
2025-01-17 17:43:43 +0100

    Room Connections Completed
    Finished the randomly generated rooms and the connections between then (needs testing) will work on the number of rooms allowed to spawn now

--- Assets/Scripts/Map_Manager.cs
+++ Assets/Scripts/Map_Manager.cs
@@ -10,14 +10,16 @@ public class MapManager : MonoBehaviour
     new Vector2(0, 0),
     new Vector2(2, 0),
     new Vector2(4, 0),
-    new Vector2(2, -2)
+    new Vector2(2, -2),
+    new Vector2(1, 1),
 };                              // Define positions for the rooms
 
 public int[,] connections = {
-    { 0, 1, 0, 0, 1 }, // Room 1 connected to Room 2
-    { 1, 0, 1, 1, 1 }, // Room 2 connected to Room 1, 3, 4
-    { 0, 1, 0, 0, 1 }, // Room 3 connected to Room 2
-    { 0, 1, 0, 0 ,1 }  // Room 4 connected to Room 2
+    { 0, 1, 0, 0, 0 }, // Room 1 connected to Room 2
+    { 1, 0, 1, 1, 0 }, // Room 2 connected to Room 1, 3, 4
+    { 0, 1, 0, 0, 1 }, // Room 3 connected to Room 2, 5
+    { 0, 1, 0, 0, 1 }, // Room 4 connected to Room 2, 5
+    { 0, 0, 1, 1, 0 }, // Room 5 connected to Room 3, 4
 };                        // Adjacency matrix for connections (1 = connected, 0 = not)
 
     private GameObject[] rooms;     // Keep track of instantiated rooms
