1f27ec680ad601f92416dee48d169dc5c037e661
Lluis Alguersuari <lluisalguersuari@gmail.com>
2025-01-21 13:08:16 +0100

    Finished connections and added visual lines, also added locks

--- Assets/Scripts/Map_Manager.cs
+++ Assets/Scripts/Map_Manager.cs
@@ -1,26 +1,134 @@
 using System.Collections.Generic;
 using UnityEngine;
+using static Room;
 
 public class MapManager : MonoBehaviour
 {
-    public GameObject[] roomPrefabs; // Array of prefabs (Battle, Shop, etc.)
-    public GameObject bossRoomPrefab; // Boss Room Prefab
+    public GameObject[] roomPrefabs; // Array of room prefabs (Battle, Shop, etc.)
+    public GameObject bossRoomPrefab; // Boss Room prefab
+    public GameObject startRoomPrefab; // Start Room prefab
     public Transform mapParent; // Parent object to hold all rooms
 
     [Header("Map Settings")]
-    public int totalRooms = 10; // Total number of rooms in the map
-    public float branchProbability = 0.4f; // Probability of branching for a new room
-    public float roomSpacing = 3f; // Minimum space between rooms
-    public float maxRoomOffset = 2f; // Maximum horizontal/vertical offset to ensure varied room placement
+    public int totalPillars = 5; // Number of vertical pillars
+    public float pillarSpacing = 5f; // Horizontal spacing between pillars
+    public int minRoomsPerPillar = 1; // Minimum number of rooms per pillar
+    public int maxRoomsPerPillar = 3; // Maximum number of rooms per pillar
+    public float verticalSpacing = 3f; // Vertical spacing between rooms within a pillar
+    public Vector2 mapMinBounds; // Minimum bounds of the map
+    public Vector2 mapMaxBounds; // Maximum bounds of the map
 
-    private List<Room> rooms; // List of all the rooms
-    private List<List<Room>> activePaths; // List of active paths (branches)
-    private Room startRoom; // Start room
-    private Room bossRoom; // Boss room
+    private List<Room> rooms; // List of all generated rooms
 
     void Start()
     {
         GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        rooms = new List<Room>();
+        List<Room> rowStash = new List<Room>();
+
+        // Step 1: Create the start room at the far-left pillar
+        Vector2 startRoomPosition = new Vector2(mapMinBounds.x, 0);
+        Room startRoom = CreateRoom(Room.RoomType.Battle, startRoomPosition, startRoomPrefab);
+        rooms.Add(startRoom);
+        rowStash.Add(startRoom);
+        startRoom.isLocked = false;
+
+        // Step 2: Generate rooms for each pillar
+        for (int pillarIndex = 1; pillarIndex <= totalPillars; pillarIndex++)
+        {
+            List<Room> currentRow = new List<Room>();
+
+            // Calculate the x-position for this pillar
+            float pillarX = mapMinBounds.x + pillarIndex * pillarSpacing;
+
+            // Generate a random number of rooms for this pillar
+            int roomCount = Random.Range(minRoomsPerPillar, maxRoomsPerPillar + 1);
+
+            // Create rooms along the y-axis for this pillar
+            for (int roomIndex = 0; roomIndex < roomCount; roomIndex++)
+            {
+                float roomY = mapMinBounds.y + (roomIndex * verticalSpacing) - ((verticalSpacing * roomCount) * 0.5f) + (verticalSpacing * 0.5f);
+                Vector2 roomPosition = new Vector2(pillarX, roomY);
+
+                // Randomly choose a room type
+                Room.RoomType roomType = GetRandomRoomType();
+                foreach (GameObject Room in roomPrefabs)
+                {
+                    if (Room.GetComponent<Room>().roomType == roomType)
+                    {
+                        // Create the room
+                        Room newRoom = CreateRoom(roomType, roomPosition, Room);
+                        int currentIndex = currentRow.Count;
+                        rooms.Add(newRoom);
+                        currentRow.Add(newRoom);
+                        while (true)
+                        {
+                            if(rowStash.Count - 1 >= currentIndex)
+                            {
+                                rowStash[currentIndex].connectedRooms.Add(newRoom);
+                                break;
+                            }
+                            else
+                                currentIndex --;    
+                        }
+                        break;
+                    }
+
+                }
+
+            }
+            foreach (Room room in rowStash)
+            {
+                if(room.connectedRooms.Count == 0)
+                {
+                    room.connectedRooms.Add(currentRow[currentRow.Count - 1]);
+                }
+            }
+            rowStash = currentRow;
+        }
+
+        // Step 3: Create the boss room at the far-right pillar
+        Vector2 bossRoomPosition = new Vector2((totalPillars + 1)* pillarSpacing, 0);
+        Room bossRoom = CreateRoom(Room.RoomType.Boss, bossRoomPosition, bossRoomPrefab);
+        rooms.Add(bossRoom);
+        foreach (Room room in rowStash)
+        {
+            room.connectedRooms.Add(bossRoom);
+        }
+
+        // Debugging output
+        Debug.Log("Map generated with total rooms: " + rooms.Count);
+    }
+
+    Room CreateRoom(Room.RoomType roomType, Vector2 position, GameObject customPrefab = null)
+    {
+        // Use the provided prefab if available, otherwise select a random prefab
+        GameObject roomPrefab = customPrefab ?? roomPrefabs[Random.Range(0, roomPrefabs.Length)];
+        GameObject roomObject = Instantiate(roomPrefab, position, Quaternion.identity, mapParent);
+
+        // Assign properties to the Room script
+        Room roomScript = roomObject.GetComponent<Room>();
+        roomScript.roomType = roomType;
+        roomScript.position = position;
+
+        // Customize the room based on type
+        CustomizeRoom(roomObject, roomType);
+
+        return roomScript;
+    }
+
+    Room.RoomType GetRandomRoomType()
+    {
+        // Randomly pick a room type (excluding None and Boss)
+        if (Random.value < 0.3f)
+        {
+            return Room.RoomType.Shop;
+        }
+        return Room.RoomType.Battle;
     }
 
     void CustomizeRoom(GameObject room, Room.RoomType roomType)
@@ -41,189 +149,5 @@ public class MapManager : MonoBehaviour
                 spriteRenderer.color = Color.black;
                 break;
         }
-    }
-
-    void GenerateMap()
-    {
-        rooms = new List<Room>();
-        activePaths = new List<List<Room>>();
-
-        // Start with the first room
-        List<Room> startPath = new List<Room>();
-        Vector2 lastRoomPosition = Vector2.zero;
-        startRoom = CreateRoom(Room.RoomType.Battle, lastRoomPosition);
-        startPath.Add(startRoom);
-        rooms.Add(startRoom);
-        activePaths.Add(startPath);
-
-        // Generate the rest of the rooms, adding branches
-        for (int i = 1; i < totalRooms - 1; i++) // Last room is always Boss, no need to generate it yet
-        {
-            // Randomly choose a path (one of the active paths)
-            List<Room> currentPath = activePaths[Random.Range(0, activePaths.Count)];
-
-            // Randomly choose a room type
-            Room.RoomType roomType = GetRandomRoomType(i);
-
-            // Get position for the new room, creating some variation in direction
-            Vector2 newRoomPosition = GetNewRoomPosition(currentPath);
-
-            // Ensure the room doesn't overlap with other rooms in the current path
-            newRoomPosition = FindValidPosition(newRoomPosition, currentPath);
-
-            // Create and place the new room
-            Room newRoom = CreateRoom(roomType, newRoomPosition);
-            rooms.Add(newRoom);
-
-            // Connect the new room to the last room in the current path
-            ConnectRooms(currentPath[currentPath.Count - 1], newRoom);
-            currentPath.Add(newRoom);
-
-            // Decide if we should branch off from this path
-            if (Random.value < branchProbability)
-            {
-                // Create a branch off the current path
-                List<Room> branchPath = new List<Room>();
-                Room branchRoom = CreateRoom(roomType, newRoomPosition + new Vector2(Random.Range(-maxRoomOffset, maxRoomOffset), Random.Range(-maxRoomOffset, maxRoomOffset)));
-                rooms.Add(branchRoom);
-                ConnectRooms(newRoom, branchRoom);
-                branchPath.Add(branchRoom);
-                activePaths.Add(branchPath); // Add the new branch to active paths
-            }
-        }
-
-        // Final Boss room
-        bossRoom = CreateRoom(Room.RoomType.Boss, activePaths[0][activePaths[0].Count - 1].position + new Vector2(3, 0));
-        rooms.Add(bossRoom);
-        //activePaths[0].Add(bossRoom);
-
-        // Connect the last room (Boss) to the previous room
-       // ConnectRooms(activePaths[0][activePaths[0].Count - 2], bossRoom);
-
-        // Ensure the Boss room has at least one path leading to it
-        EnsureBossRoomConnection();
-
-        // Debugging output
-        Debug.Log("Map Generated with rooms: " + rooms.Count);
-    }
-
-    Room CreateRoom(Room.RoomType roomType, Vector2 position)
-    {
-        GameObject roomPrefab = GetRoomPrefab(roomType);
-        GameObject roomObject = Instantiate(roomPrefab, position, Quaternion.identity, mapParent);
-        Room roomScript = roomObject.GetComponent<Room>();
-
-        roomScript.roomType = roomType;
-        roomScript.position = position;
-
-        // Customize the room based on type
-        CustomizeRoom(roomObject, roomType);
-
-        return roomScript;
-    }
-
-    GameObject GetRoomPrefab(Room.RoomType roomType)
-    {
-        switch (roomType)
-        {
-            case Room.RoomType.Boss:
-                return bossRoomPrefab;
-            default:
-                return roomPrefabs[Random.Range(0, roomPrefabs.Length)];
-        }
-    }
-
-    Room.RoomType GetRandomRoomType(int index)
-    {
-        // Randomly pick a room type, but ensure the last room is always the Boss
-        if (index == totalRooms - 1)
-        {
-            return Room.RoomType.Boss;
-        }
-        else if (Random.value < 0.3f)
-        {
-            return Room.RoomType.Shop;
-        }
-        else
-        {
-            return Room.RoomType.Battle;
-        }
-    }
-
-    void ConnectRooms(Room roomA, Room roomB)
-    {
-        // Connect two rooms bidirectionally
-        roomA.connectedRooms = AddToArray(roomA.connectedRooms, roomB);
-        roomB.connectedRooms = AddToArray(roomB.connectedRooms, roomA);
-    }
-
-    Room[] AddToArray(Room[] array, Room item)
-    {
-        List<Room> list = new List<Room>(array);
-        list.Add(item);
-        return list.ToArray();
-    }
-
-    Vector2 GetNewRoomPosition(List<Room> currentPath)
-    {
-        // Create variation by adding small random offsets
-        Room lastRoom = currentPath[currentPath.Count - 1];
-        Vector2 lastRoomPosition = lastRoom.position;
-        return lastRoomPosition + new Vector2(2, Random.Range(-1f, 1f)); // slight vertical variation
-    }
-
-    Vector2 FindValidPosition(Vector2 desiredPosition, List<Room> currentPath)
-    {
-        // Try different positions until we find one that doesn't overlap with other rooms in the current path
-        Vector2 offset = Vector2.zero;
-        int maxAttempts = 50;
-
-        for (int i = 0; i < maxAttempts; i++)
-        {
-            bool isValid = true;
-
-            // Only check for collisions with rooms in the current path
-            foreach (Room room in currentPath)
-            {
-                if (Vector2.Distance(room.position, desiredPosition + offset) < roomSpacing)
-                {
-                    isValid = false;
-                    break;
-                }
-            }
-
-            if (isValid)
-            {
-                return desiredPosition + offset;
-            }
-            else
-            {
-                offset = new Vector2(Random.Range(-maxRoomOffset, maxRoomOffset), Random.Range(-maxRoomOffset, maxRoomOffset)); // Add small random offset
-            }
-        }
-
-        // If no valid position found, return the original position
-        return desiredPosition;
-    }
-
-    // Ensure the Boss room is connected and not a dead-end
-    void EnsureBossRoomConnection()
-    {
-
-        foreach( List<Room> path in activePaths)
-        {
-            Room lastRoom = path[path.Count - 1];
-         
-            ConnectRooms(lastRoom, bossRoom);
-        }
-        /*
-        // Find a room that's connected to the boss room, if it's not connected to any, create a path to it
-        if (bossRoom.connectedRooms.Length == 0)
-        {
-            // Pick a random room and connect it to the boss room
-            Room randomRoom = rooms[Random.Range(0, rooms.Count)];
-            ConnectRooms(randomRoom, bossRoom);
-        }
-        */
     }
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,42 +9,58 @@ public class Room : MonoBehaviour
 
     [Header("Room Settings")]
     public Vector2 position; // Position in the map of each room to place them 
-    public Room[] connectedRooms; // Array of connected rooms
+    public List<Room> connectedRooms = new List<Room>(); // Array of connected rooms
+    public GameObject linePrefab; // Reference to a prefab containing a LineRenderer
+    public bool isLocked = true; //set the lock for the rooms
 
-    void OnDrawGizmos()
+    void Start()
     {
-        // Draw lines to connected rooms in the editor for debugging 
-        if (connectedRooms != null)
+        DrawConnections();
+    }
+
+    void DrawConnections()
+    {
+        foreach (var connectedRoom in connectedRooms)
         {
-            Gizmos.color = Color.black;
-            foreach (var connectedRoom in connectedRooms)
+            if (connectedRoom != null)
             {
-                if (connectedRoom != null)
-                {
-                    Gizmos.DrawLine(transform.position, connectedRoom.transform.position);
-                }
+                // Instantiate a new line object
+                GameObject lineObject = Instantiate(linePrefab, transform.position, Quaternion.identity);
+                LineRenderer lineRenderer = lineObject.GetComponent<LineRenderer>();
+
+                // Set the start and end points of the line
+                lineRenderer.positionCount = 2;
+                lineRenderer.SetPosition(0, transform.position);
+                lineRenderer.SetPosition(1, connectedRoom.transform.position);
             }
         }
     }
 
     private void OnMouseDown()
     {
-        Debug.Log($"Entered {roomType} room at position {position}");
-
-        switch (roomType)
+        if (isLocked == false)
         {
-            case RoomType.Battle:
-                SceneManager.LoadScene("BattleScene");
-                break;
-            case RoomType.Shop:
-                SceneManager.LoadScene("ShopScene");
-                break;
-            case RoomType.Boss:
-                SceneManager.LoadScene("BossRoom");
-                break;
-            default:
-                Debug.LogError($"Room at position {position} has an invalid or unassigned RoomType!");
-                break;
+            Debug.Log($"Entered {roomType} room at position {position}");
+
+            switch (roomType)
+            {
+                case RoomType.Battle:
+                    SceneManager.LoadScene("TheScene");
+                    break;
+                case RoomType.Shop:
+                    SceneManager.LoadScene("ShopScene");
+                    break;
+                case RoomType.Boss:
+                    SceneManager.LoadScene("BossRoom");
+                    break;
+                default:
+                    Debug.LogError($"Room at position {position} has an invalid or unassigned RoomType!");
+                    break;
+            }
+        }
+        else if (isLocked == true)
+        {
+            Debug.Log($"The Room {roomType} is locked");
         }
     }
 }
